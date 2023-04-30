9aab92bed786036ca0a5d7e384ee6110f7b2ccd9
dafermiso <df.guerrerov1@uniandes.edu.co>
2023-04-30 16:15:26 -0500

    Adición launch screen activity

---
+++ Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/launch_screen_activity.kt
@@ -0,0 +1,11 @@
+package co.edu.uniandes.vinylsg12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class launch_screen_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launch_screen_activity)
+    }
+}
