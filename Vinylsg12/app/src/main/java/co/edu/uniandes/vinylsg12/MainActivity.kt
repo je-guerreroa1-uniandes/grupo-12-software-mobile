6b2188a6f5a324671267c232bf5bfdb9adda3258
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-05-10 02:37:02 -0500

    feat: Add navigation drawer

--- Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/MainActivity.kt
+++ Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/MainActivity.kt
@@ -1,25 +1,48 @@
 package co.edu.uniandes.vinylsg12
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.Window
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import co.edu.uniandes.vinylsg12.databinding.ActivityMainBinding
+import co.edu.uniandes.vinylsg12.ui.about_us.AboutActivity
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         val navView: BottomNavigationView = binding.navView
+        drawerLayout = binding.drawerLayout
+//        toggle = ActionBarDrawerToggle(
+//            this,
+//            drawerLayout,
+//            binding.toolbar,
+//            R.string.navigation_drawer_open,
+//            R.string.navigation_drawer_close
+//        )
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+
+        val navMenuView = binding.navMenuView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -31,5 +54,56 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navMenuView.setNavigationItemSelectedListener(this)
+
+//        val drawerToggle = ActionBarDrawerToggle(
+//            this,
+//            binding.drawerLayout,
+//            binding.toolbar,
+//            R.string.navigation_drawer_open,
+//            R.string.navigation_drawer_close
+//        )
+//
+//        binding.drawerToggle.setOnClickListener {
+//            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//                binding.drawerLayout.closeDrawer(GravityCompat.START)
+//            } else {
+//                binding.drawerLayout.openDrawer(GravityCompat.START)
+//            }
+//        }
+//
+//        binding.drawerLayout.addDrawerListener(drawerToggle)
+//        drawerToggle.syncState()
+       toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId == R.id.action_about_us) {
+            AboutActivity.start(context = this)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawer(GravityCompat.START)
+        when(item.itemId) {
+            R.id.action_about_us -> AboutActivity.start(this)
+        }
+        return true
     }
 }
