a621968248d431f7595a785623c8dcafe117e3b0
dafermiso <df.guerrerov1@uniandes.edu.co>
2023-05-13 14:12:12 -0500

    activity crear album

---
+++ Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/home_albums_activity.kt
@@ -0,0 +1,11 @@
+package co.edu.uniandes.vinylsg12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_albums_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_albums_activity)
+    }
+}
