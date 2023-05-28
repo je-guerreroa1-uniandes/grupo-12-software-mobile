f4a851b36d7ad85c59e0992709ae7929acfeab19
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-05-28 00:26:08 -0500

    feat: Add album to collector's albums

--- Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/ui/collectors/collected_albums/CollectedAlbumsActivity.kt
+++ Vinylsg12/app/src/main/java/co/edu/uniandes/vinylsg12/ui/collectors/collected_albums/CollectedAlbumsActivity.kt
@@ -5,27 +5,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.edu.uniandes.vinylsg12.common.api.models.Album
 import co.edu.uniandes.vinylsg12.databinding.ActivityCollectedAlbumsBinding
-import co.edu.uniandes.vinylsg12.ui.albums.adapters.AlbumAdapter
+import co.edu.uniandes.vinylsg12.databinding.DialogAddToCollectionBinding
 import co.edu.uniandes.vinylsg12.ui.collectors.collected_albums.adapters.CollectedAlbumsAdapter
 
 class CollectedAlbumsActivity : AppCompatActivity() {
 
     private var _binding: ActivityCollectedAlbumsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: CollectedAlbumsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel =
+        viewModel =
             ViewModelProvider(this).get(CollectedAlbumsViewModel::class.java)
         _binding = ActivityCollectedAlbumsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.addAlbumButton.setOnClickListener {
+            viewModel.fetchAlbums()
+        }
         subscribeToVariables(viewModel)
-        viewModel.fetchData()
+        viewModel.fetchCollectedAlbums()
     }
 
     companion object {
@@ -45,5 +52,55 @@ class CollectedAlbumsActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
             binding.progressBar.visibility = View.GONE
         }
+        viewModel.albums.observe(this) {
+            showAddAlbumDialog(it)
+        }
+    }
+
+    private fun showAddAlbumDialog(albums: List<Album>) {
+        val dialogBinding = DialogAddToCollectionBinding.inflate(layoutInflater)
+        val dialogView = dialogBinding.root
+        // Inside your activity or fragment
+
+// Create an AlertDialog.Builder object
+        val builder = AlertDialog.Builder(this)
+            .setTitle("Add Album to Collection")
+            .setView(dialogView)
+
+        val spinnerAlbum = dialogBinding.spinnerAlbum
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, albums.map { it.name })
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerAlbum.adapter = adapter
+
+// Set a positive button click listener
+        builder.setPositiveButton("Add") { dialog, _ ->
+            // Get the price value from the input field and convert it to Int
+            val priceString = dialogBinding.price.text.toString()
+            val price = priceString.toIntOrNull()
+            // Retrieve the selected album
+            val selectedAlbum = albums[spinnerAlbum.selectedItemPosition]
+
+            if (price != null) {
+                // Add the selected album to your collection or perform any desired operations
+                viewModel.addToCollection(
+                    albumId = selectedAlbum.id ?: 0,
+                    price = price,
+                    onSuccess = {
+                        // Dismiss the dialog
+                        dialog.dismiss()
+                        viewModel.fetchCollectedAlbums()
+                    }
+                ) {
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Invalid price format: $priceString", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // Create and show the dialog
+        val dialog = builder.create()
+        dialog.show()
+
     }
 }
